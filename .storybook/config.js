bd190d44f50c99e4f5f5753a8e90ad37a045edd4
Adam Bulmer <mintuz1990@gmail.com>
2019-09-29 17:18:28 +0100

    Fix sort order

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,2 +1,7 @@
-import { configure } from '@storybook/react'
+import { addParameters, configure } from '@storybook/react'
+addParameters({
+    options: {
+        storySort: (a, b) => a[1].id.localeCompare(b[1].id),
+    },
+})
 configure(require.context('../stories/', true, /\.stories\.(js|mdx)$/), module)
