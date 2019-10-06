fd52ddebcd20affb8fcb0d20fb2c333b46d4bb82
Adam Bulmer <mintuz1990@gmail.com>
2019-10-06 10:05:06 +0100

    Button as typescript

---
+++ src/Components/Button.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+import { variant } from 'styled-system'
+import css from '@styled-system/css'
+
+type ButtonProps = {
+    /** Called each time the button is clicked */
+    onClick?: () => void
+    /** Variant of button to render. */
+    variant?: string
+}
+
+const StyledButton = styled.button`
+    display: inline-flex;
+    cursor: pointer;
+    border: none;
+    margin: 0;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+
+    ${css({
+        padding: [4],
+    })}
+
+    ${variant({
+        scale: 'button',
+    })}
+`
+
+const Button: FC<ButtonProps> = ({
+    children,
+    variant = 'primary',
+    onClick = () => {},
+}) => {
+    return (
+        <StyledButton variant={variant} onClick={onClick}>
+            {children}
+        </StyledButton>
+    )
+}
+
+export default Button
