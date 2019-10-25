f63db14fcf6530ef8d82376e076e1924b0227abb
Adam Bulmer <mintuz1990@gmail.com>
2019-10-25 18:39:48 +0100

    Fix build

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,6 +1,17 @@
+import styled from 'styled-components'
 import { useIntersect } from '@mintuz/react-intersect'
 
-export default ({ src, alt = '', lazy = true }) => {
+const Placeholder = styled.div`
+    display: block;
+    overflow: hidden;
+    height: 0;
+    padding-bottom: ${props => {
+        const parts = props.aspect.split(':')
+        return (parts[1] / parts[0]) * 100
+    }};
+`
+
+export default ({ src, alt = '', lazy = true, aspect = '16:9' }) => {
     const [inView, ref] = useIntersect({
         triggerOnce: true,
     })
@@ -8,7 +19,11 @@ export default ({ src, alt = '', lazy = true }) => {
     if (lazy) {
         return (
             <div ref={ref}>
-                {inView ? <img src={src} alt={alt}></img> : <div />}
+                {inView ? (
+                    <img src={src} alt={alt}></img>
+                ) : (
+                    <Placeholder aspect={aspect} />
+                )}
             </div>
         )
     }
