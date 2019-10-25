8604c44db5373ba7cbf11d7bdd63e02889cfb1f5
Adam Bulmer <mintuz1990@gmail.com>
2019-10-25 19:06:36 +0100

    Image enhancements

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,30 +1,56 @@
+import React from 'react'
 import styled from 'styled-components'
 import { useIntersect } from '@mintuz/react-intersect'
 
+const ImageContainer = styled.div`
+    img {
+        display: block;
+    }
+`
+
 const Placeholder = styled.div`
+    position: relative;
     display: block;
     overflow: hidden;
     height: 0;
     padding-bottom: ${props => {
         const parts = props.aspect.split(':')
-        return (parts[1] / parts[0]) * 100
+        return `${(parts[1] / parts[0]) * 100}%`
     }};
 `
 
-export default ({ src, alt = '', lazy = true, aspect = '16:9' }) => {
+const PlaceholderContent = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+`
+
+export default ({
+    src,
+    alt = '',
+    loading = 'auto',
+    aspect = '16:9',
+    children,
+}) => {
     const [inView, ref] = useIntersect({
         triggerOnce: true,
     })
 
-    if (lazy) {
+    if (loading === 'lazy') {
         return (
-            <div ref={ref}>
+            <ImageContainer ref={ref}>
                 {inView ? (
                     <img src={src} alt={alt}></img>
                 ) : (
-                    <Placeholder aspect={aspect} />
+                    <Placeholder aspect={aspect}>
+                        {children && (
+                            <PlaceholderContent>{children}</PlaceholderContent>
+                        )}
+                    </Placeholder>
                 )}
-            </div>
+            </ImageContainer>
         )
     }
 
