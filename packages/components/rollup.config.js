e3f83a2a70c3c421ce7b9ce5bb2a7c6d7f9ae77a
Adam Bulmer <mintuz1990@gmail.com>
2022-04-22 22:28:12 +0100

    feat: update turbo.json

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -6,7 +6,7 @@ import { nodeResolve } from '@rollup/plugin-node-resolve'
 const packageJson = require('./package.json')
 
 export default {
-    input: 'src/index.ts',
+    input: './src/index.ts',
     output: [
         {
             file: packageJson.main,
