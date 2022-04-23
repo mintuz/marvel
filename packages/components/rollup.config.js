cd770c99e0247e3469a11e28564a8600e0ae586c
Adam Bulmer <mintuz1990@gmail.com>
2022-04-23 18:16:50 +0100

    feat: rollup config changes

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,6 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
-import { resolve } from '@rollup/plugin-node-resolve'
+import { nodeResolve as resolve } from '@rollup/plugin-node-resolve'
 
 const packageJson = require('./package.json')
 
