1395e757aecc8d5636aa4b2031fe518ae16918ef
Adam Bulmer <mintuz1990@gmail.com>
2022-05-06 11:42:39 +0100

    feat: cluster accepts alignment

--- packages/components/src/layout/Cluster.tsx
+++ packages/components/src/layout/Cluster.tsx
@@ -1,15 +1,16 @@
 import styled from 'styled-components'
-import { system, layout, LayoutProps } from 'styled-system'
+import { system, layout, LayoutProps, variant } from 'styled-system'
 
 type ClusterProps = {
     /** Spacing unit defined within theme object */
     spacing: number
+    /** Horizontal alignment */
+    alignment: 'start' | 'center' | 'end'
 } & LayoutProps
 
 export const Cluster = styled.div<ClusterProps>`
     display: flex;
     flex-wrap: wrap;
-    justify-content: flex-start;
     align-items: center;
     ${system({
         spacing: {
@@ -18,8 +19,23 @@ export const Cluster = styled.div<ClusterProps>`
         },
     })}
     ${layout}
+    ${variant({
+        prop: 'alignment',
+        variants: {
+            start: {
+                justifyContent: 'flex-start',
+            },
+            center: {
+                justifyContent: 'center',
+            },
+            end: {
+                justifyContent: 'flex-end',
+            },
+        },
+    })}
 `
 
 Cluster.defaultProps = {
     spacing: 4,
+    alignment: 'start',
 }
